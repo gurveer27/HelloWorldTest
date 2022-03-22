10673e7b5d1edd50e77dd5474c6a2a9e5d8a0762
Gurveer Saund <gsaund@uoguelph.ca>
2022-03-22 16:53:57 -0400

    added print line statement

---
+++ src/helloworldtest/HelloWorldTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworldtest;
+
+/**
+ *
+ * @author gurve
+ */
+public class HelloWorldTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
